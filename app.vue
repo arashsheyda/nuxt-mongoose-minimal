9bfb0eabd16b44aeb02a9ff5c7576018cde832eb
Arash <38922203+arashsheyda@users.noreply.github.com>
2023-10-18 15:19:58 -0600

    feat(connection): set interval

--- app.vue
+++ app.vue
@@ -1,5 +1,21 @@
 <script setup lang="ts">
-const { data: readyState } = await useFetch('/api/status')
+import { useIntervalFn } from '@vueuse/core'
+
+const { data, refresh } = await useFetch('/api/status')
+const readyState = ref(data.value ?? 0)
+
+const { pause, resume } = useIntervalFn(async () => {
+  refresh()
+  readyState.value = data.value ?? 0
+  console.log('re-checking...')
+}, 5000)
+
+watch((readyState), () => {
+  if (readyState.value === 1)
+    pause()
+  else
+    resume()
+}, { immediate: true })
 </script>
 
 <template>
